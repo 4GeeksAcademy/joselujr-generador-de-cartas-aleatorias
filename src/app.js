998c601e2af6d9bc722112323224fc125bb37fcb
Jose Rodriguez <masullo.joseluis@gmail.com>
2026-06-29 20:13:52 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -9,20 +9,23 @@ window.onload = function () {
   //write your code here
   let numeros = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A",]
   let palos = ["♦", "♥", "♠", "♣"]
-  let numerosRandom = Math.floor(Math.random() * numeros.length);
-  let palosRandom = Math.floor(Math.random() * palos.length);
-  let resNumeros = numeros[numerosRandom];
-  let resPalos = palos[palosRandom]
-  if (resPalos === "♥" || resPalos === "♦") {
-    document.getElementById("palo-superior").style.color = "red";
-    document.getElementById("palo-inferior").style.color = "red";
-  } else {
-    document.getElementById("palo-superior").style.color = "black";
-    document.getElementById("palo-inferior").style.color = "black";
+  function nuevaCarta() {
+    let numerosRandom = Math.floor(Math.random() * numeros.length);
+    let palosRandom = Math.floor(Math.random() * palos.length);
+    let resNumeros = numeros[numerosRandom];
+    let resPalos = palos[palosRandom]
+    if (resPalos === "♥" || resPalos === "♦") {
+      document.getElementById("palo-superior").style.color = "red";
+      document.getElementById("palo-inferior").style.color = "red";
+    } else {
+      document.getElementById("palo-superior").style.color = "black";
+      document.getElementById("palo-inferior").style.color = "black";
+    }
+    document.getElementById("palo-superior").innerHTML = resPalos;
+    document.getElementById("letra").innerHTML = resNumeros;
+    document.getElementById("palo-inferior").innerHTML = resPalos;
   }
-
-  document.getElementById("palo-superior").innerHTML = resPalos;
-  document.getElementById("numero").innerHTML = resNumeros;
-  document.getElementById("palo-inferior").innerHTML = resPalos;
-  
+  nuevaCarta();
+  let boton = document.getElementById("cambiarCarta");
+  boton.addEventListener("click", nuevaCarta)
 };
