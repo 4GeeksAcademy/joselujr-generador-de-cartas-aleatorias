26a08189e1f291919a2ceed1174e8b047c291897
Jose Rodriguez <masullo.joseluis@gmail.com>
2026-06-29 19:45:57 +0000

    casi terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numeros = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A",]
+  let palos = ["♦", "♥", "♠", "♣"]
+  let numerosRandom = Math.floor(Math.random() * numeros.length);
+  let palosRandom = Math.floor(Math.random() * palos.length);
+  let resNumeros = numeros[numerosRandom];
+  let resPalos = palos[palosRandom]
+  if (resPalos === "♥" || resPalos === "♦") {
+    document.getElementById("palo-superior").style.color = "red";
+    document.getElementById("palo-inferior").style.color = "red";
+  } else {
+    document.getElementById("palo-superior").style.color = "black";
+    document.getElementById("palo-inferior").style.color = "black";
+  }
+
+  document.getElementById("palo-superior").innerHTML = resPalos;
+  document.getElementById("numero").innerHTML = resNumeros;
+  document.getElementById("palo-inferior").innerHTML = resPalos;
+  
 };
